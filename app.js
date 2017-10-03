87389ef529cba1f4c5c79f0f1a063f9e4833c683
deepakneo <dev.deepak.deshmukh@gmail.com>
2017-10-03 13:01:40 +0530

    limit the no of hits by particular IP

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ var count = 0;
 const app = express();
 
 console.log(path.resolve(__dirname,'..'));
-//const router = express.Router;
+
 // set variables 
 app.set('port', process.env.port || 3000);
 app.set('view engine','html');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser('santosh'));
